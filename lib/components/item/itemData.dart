bca198b529a05a294011d43f6fedc3db63bba11c
long <tranthoilong.giaitri@gmail.com>
2023-04-12 08:17:42 +0700

    up

---
+++ lib/components/item/itemData.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: camel_case_types
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class itemData extends StatelessWidget {
+  const itemData({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 10,
+        left: 16,
+        right: 16,
+      ),
+      child: Container(
+        width: size.width,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: BackdropFilter(
+          blendMode: BlendMode.color,
+          filter: ImageFilter.blur(
+            sigmaX: 10,
+            sigmaY: 10,
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(.2),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/modules/home/layouts/pages/profie_page.dart
+++ lib/modules/home/layouts/pages/profie_page.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: deprecated_member_use
 
-import 'dart:ui';
-
-import 'package:app/components/style/textstyle.dart';
+import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:image_picker/image_picker.dart';
+import '../../../../components/item/itemData.dart';
 
 class ProFilePage extends StatefulWidget {
   const ProFilePage({super.key});
@@ -15,19 +12,132 @@ class ProFilePage extends StatefulWidget {
 }
 
 class _ProFilePageState extends State<ProFilePage> {
-  File? _imageFile;
-  void _openGallery() async {
-    final pickedFile =
-        await ImagePicker().getImage(source: ImageSource.gallery);
-    setState(() {
-      _imageFile = File(pickedFile!.path);
-    });
+  List<User>? selectedUserList = [];
+
+  Future<void> openFilterDelegate() async {
+    await FilterListDelegate.show<User>(
+      context: context,
+      list: userList,
+      selectedListData: selectedUserList,
+      theme: FilterListDelegateThemeData(
+        listTileTheme: ListTileThemeData(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          tileColor: Colors.white,
+          selectedColor: Colors.red,
+          selectedTileColor: const Color(0xFF649BEC).withOpacity(.5),
+          textColor: Colors.blue,
+        ),
+      ),
+      // enableOnlySingleSelection: true,
+      onItemSearch: (user, query) {
+        return user.name!.toLowerCase().contains(query.toLowerCase());
+      },
+      tileLabel: (user) => user!.name,
+      emptySearchChild: const Center(child: Text('No user found')),
+      // enableOnlySingleSelection: true,
+      searchFieldHint: 'Search Here..',
+      /*suggestionBuilder: (context, user, isSelected) {
+        return ListTile(
+          title: Text(user.name!),
+          leading: const CircleAvatar(
+            backgroundColor: Colors.blue,
+          ),
+          selected: isSelected,
+        );
+      },*/
+      onApplyButtonClick: (list) {
+        setState(() {
+          selectedUserList = list;
+        });
+      },
+    );
+  }
+
+  Future<void> _openFilterDialog() async {
+    await FilterListDialog.display<User>(
+      context,
+      hideSelectedTextCount: true,
+      themeData: FilterListThemeData(context),
+      headlineText: 'Select Users',
+      height: 500,
+      listData: userList,
+      selectedListData: selectedUserList,
+      choiceChipLabel: (item) => item!.name,
+      validateSelectedItem: (list, val) => list!.contains(val),
+      controlButtons: [ControlButtonType.All, ControlButtonType.Reset],
+      onItemSearch: (user, query) {
+        return user.name!.toLowerCase().contains(query.toLowerCase());
+      },
+      onApplyButtonClick: (list) {
+        setState(() {
+          selectedUserList = List.from(list!);
+        });
+        Navigator.pop(context);
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            TextButton(
+              onPressed: () async {
+                final list = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FilterPage(
+                      allTextList: userList,
+                      selectedUserList: selectedUserList,
+                    ),
+                  ),
+                );
+                if (list != null) {
+                  setState(() {
+                    selectedUserList = List.from(list);
+                  });
+                }
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+              ),
+              child: const Text(
+                "Filter Page",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextButton(
+              onPressed: _openFilterDialog,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+              ),
+              child: const Text(
+                "Filter Dialog",
+                style: TextStyle(color: Colors.white),
+              ),
+              // color: Colors.blue,
+            ),
+            TextButton(
+              onPressed: openFilterDelegate,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+              ),
+              child: const Text(
+                "Filter Delegate",
+                style: TextStyle(color: Colors.white),
+              ),
+              // color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
       body: Container(
         width: size.width,
         height: size.height,
@@ -36,32 +146,74 @@ class _ProFilePageState extends State<ProFilePage> {
               image: AssetImage('assets/images/background_image.png'),
               fit: BoxFit.cover),
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: List.generate(
-                10,
-                (index) => Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Container(
-                        width: size.width,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: BackdropFilter(
-                          // blendMode :BlendMode.lighten ,
-                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(.2),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                    )),
-          ),
+        child: Column(
+          children: [
+            Flexible(child: Container()),
+            Flexible(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: List.generate(20, (index) => const itemData()),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class User {
+  final String? name;
+  final String? avatar;
+  User({this.name, this.avatar});
+}
+
+List<User> userList = [
+  User(name: "Jon", avatar: ""),
+  User(name: "Lindsey ", avatar: ""),
+  User(name: "Valarie ", avatar: ""),
+  User(name: "Elyse ", avatar: ""),
+  User(name: "Ethel ", avatar: ""),
+  User(name: "Emelyan ", avatar: ""),
+  User(name: "Catherine ", avatar: ""),
+  User(name: "Stepanida  ", avatar: ""),
+  User(name: "Carolina ", avatar: ""),
+  User(name: "Nail  ", avatar: ""),
+  User(name: "Kamil ", avatar: ""),
+  User(name: "Mariana ", avatar: ""),
+  User(name: "Katerina ", avatar: ""),
+];
+
+class FilterPage extends StatelessWidget {
+  const FilterPage({Key? key, this.allTextList, this.selectedUserList})
+      : super(key: key);
+  final List<User>? allTextList;
+  final List<User>? selectedUserList;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Filter list Page"),
+      ),
+      body: SafeArea(
+        child: FilterListWidget<User>(
+          themeData: FilterListThemeData(context),
+          hideSelectedTextCount: true,
+          listData: userList,
+          selectedListData: selectedUserList,
+          onApplyButtonClick: (list) {
+            Navigator.pop(context, list);
+          },
+          choiceChipLabel: (item) {
+            return item!.name;
+          },
+          validateSelectedItem: (list, val) {
+            return list!.contains(val);
+          },
+          onItemSearch: (user, query) {
+            return user.name!.toLowerCase().contains(query.toLowerCase());
+          },
         ),
       ),
     );
