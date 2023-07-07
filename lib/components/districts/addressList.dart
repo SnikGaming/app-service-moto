88403bc816e8c39ed23e6ad12aeea27df7810db1
long <tranthoilong.giaitri@gmail.com>
2023-07-07 16:14:05 +0700

    up

--- lib/components/districts/addressList.dart
+++ lib/components/districts/addressList.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: file_names, library_prefixes, use_key_in_widget_constructors, library_private_types_in_public_api
 
+import 'package:app/components/button/mybutton.dart';
+import 'package:app/components/calendar/res/colors.dart';
+import 'package:app/components/style/text_style.dart';
+import 'package:app/functions/random_color.dart';
 import 'package:flutter/material.dart';
 import '../../modules/home/api/address/api_address.dart';
 import '../../modules/home/api/address/model.dart' as Address;
@@ -43,87 +47,172 @@ class _AddressListScreenState extends State<AddressListScreen> {
         itemBuilder: (context, index) {
           if (index == addresses.length) {
             // Last item, display the "Add Address" button
-            return ListTile(
-              title: const Text(txtAddAddress),
-              leading: const Icon(Icons.add),
-              onTap: () {
-                // addressLocation(context: context).then((value) => loadData());
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => FormInputLocation(
-                              data: APILocation.dataLocation,
-                            ))).then((value) => loadData());
-                // dangGuiMail(context);
-                // Implement add address functionality
-              },
+            // return ListTile(
+            //   title: const Text(txtAddAddress),
+            //   leading: const Icon(Icons.add),
+            //   onTap: () {
+            //     // addressLocation(context: context).then((value) => loadData());
+            //     Navigator.push(
+            //         context,
+            //         MaterialPageRoute(
+            //             builder: (context) => FormInputLocation(
+            //                   data: APILocation.dataLocation,
+            //                 ))).then((value) => loadData());
+            //   },
+            // );
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FormInputLocation(
+                                    data: APILocation.dataLocation,
+                                  ))).then((value) => loadData());
+                    },
+                    child: Container(
+                      height: 48,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              'Thêm địa chỉ',
+                              style: title2.copyWith(fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(),
+                ],
+              ),
             );
           } else {
             final address = addresses[index];
-            return ListTile(
-              tileColor: const Color.fromRGBO(102, 21, 207, 0.571),
-              title: Text(
-                address.name!,
-                style: h1.copyWith(
-                  color: Colors.white,
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                constraints: const BoxConstraints(minHeight: 100),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 14, 135, 151),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15, bottom: 15),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                text: 'Người nhận : ',
+                                style: title2.copyWith(fontSize: 18),
+                                children: [
+                                  TextSpan(
+                                      text: address.name!,
+                                      style: title2.copyWith(fontSize: 16)),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                text: 'Số điện thoại : ',
+                                style: title2.copyWith(fontSize: 18),
+                                children: [
+                                  TextSpan(
+                                      text: address.phoneNumber!,
+                                      style: title2.copyWith(fontSize: 16)),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                text: 'Địa chỉ nhận : ',
+                                style: title2.copyWith(fontSize: 18),
+                                children: [
+                                  TextSpan(
+                                      text:
+                                          '${address.address}, ${address.ward}, ${address.district}, ${address.province}',
+                                      style: title2.copyWith(fontSize: 16)),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  child: const Icon(Icons.edit,
+                                      color: Colors.yellowAccent),
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => FormInputLocation(
+                                          data: APILocation.dataLocation,
+                                          defaultProvinceId:
+                                              address.idProvince!,
+                                          defaultDistrictId:
+                                              address.idDistrict!,
+                                          defaultWardId: address.idWard!,
+                                          defaultName: address.name!,
+                                          defaultAddress: address.address!,
+                                          defaultPhone: address.phoneNumber!,
+                                          id: address.id,
+                                        ),
+                                      ),
+                                    ).then((value) => loadData());
+                                  },
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                GestureDetector(
+                                  child: const Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                  ),
+                                  onTap: () {
+                                    APIAddress.deleteAddress(address.id!);
+                                    loadData();
+                                    // Implement delete address functionality
+                                  },
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
                 ),
               ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    address.phoneNumber!,
-                    style: h1.copyWith(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    '${address.address}, ${address.ward}, ${address.district}, ${address.province}',
-                    style: h1.copyWith(
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.edit, color: Colors.yellowAccent),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FormInputLocation(
-                            data: APILocation.dataLocation,
-                            defaultProvinceId: address.idProvince!,
-                            defaultDistrictId: address.idDistrict!,
-                            defaultWardId: address.idWard!,
-                            defaultName: address.name!,
-                            defaultAddress: address.address!,
-                            defaultPhone: address.phoneNumber!,
-                            id: address.id,
-                          ),
-                        ),
-                      ).then((value) => loadData());
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    ),
-                    onPressed: () {
-                      APIAddress.deleteAddress(address.id!);
-                      loadData();
-                      // Implement delete address functionality
-                    },
-                  ),
-                ],
-              ),
-              onTap: () {
-                Navigator.pop(context, address);
-              },
             );
           }
         },
